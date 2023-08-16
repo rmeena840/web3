758d741a042c29f4f4c0021926ff7466d0cdfffe
Ravinda <ravindra.iitism@gmail.com>
2023-08-16 17:14:14 +0530

    ganache and web3 setup

---
+++ test/Inbox.test.js
@@ -0,0 +1,14 @@
+const ganache = require('ganache-cli');
+const { Web3 } = require('web3');
+const web3 = new Web3(ganache.provider());
+
+beforeEach(async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+})
+
+describe('Inbox contract tests', () => {
+    it('should get accounts list', () => {
+
+    })
+})

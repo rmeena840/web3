ffcdb55e375ba41e19bbf02ec131c8c6cef955d8
Ravinda <ravindra.iitism@gmail.com>
2023-08-23 20:03:42 +0530

    feat: contract deploy added

---
+++ ether.deploy.js
@@ -0,0 +1,20 @@
+const { interface, bytecode } = require('./compile.ts');
+const fs = require("fs-extra")
+const ethers = require('ethers');
+
+const RPC_URL = 'http://127.0.0.1:8545'
+
+const main = async () => {
+    const provider = new ethers.JsonRpcProvider(RPC_URL);
+    const wallet = new ethers.Wallet("0x0047f9d986496a7c7b9391d708994e25eb492c28ecf6a6ca93dd536b8c7fd6d9", provider);
+
+    const contractFactory = new ethers.ContractFactory(interface, bytecode, wallet);
+    console.log("Deploying...")
+    const contract = await contractFactory.deploy("Ravindra");
+    const txnReceipt = await contract.waitForDeployment();
+    console.log("Deployment txn: ", contract.deploymentTransaction());
+    console.log("txn receipt: ", txnReceipt)
+    console.log("Contract Deployed:", await contract.getAddress());
+}
+
+main()
